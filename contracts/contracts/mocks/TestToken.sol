f793a92ded538b48c5d5923043390ee32c1016df
Matias Zapata <matuzapata@gmail.com>
2025-03-23 02:20:27 -0300

    redeploy + permit

--- contracts/contracts/mocks/TestToken.sol
+++ contracts/contracts/mocks/TestToken.sol
@@ -2,9 +2,13 @@
 pragma solidity ^0.8.24;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract TestToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("TestToken", "TEST") {
+contract TestToken is ERC20, ERC20Permit {
+    constructor(
+        uint256 initialSupply
+    ) ERC20("TestToken", "TEST") ERC20Permit("TestToken") {
         _mint(msg.sender, initialSupply);
     }
 
--- frontend/app/connection/page.tsx
+++ frontend/app/connection/page.tsx
@@ -44,355 +44,25 @@ export default function Connection() {
       localStorage.setItem("publicKey", publicKey);
       localStorage.setItem("privateKey", privateKey);
 
-      const approve = await MiniKit.commandsAsync.sendTransaction({
-        transaction: [
-          {
-            address: "0xd242da12AacBeC695Fe91903E8c67Bbe6E1dF4a7",
-            abi: [
-              {
-                "inputs": [
-                  {
-                    "internalType": "uint256",
-                    "name": "initialSupply",
-                    "type": "uint256"
-                  }
-                ],
-                "stateMutability": "nonpayable",
-                "type": "constructor"
-              },
-              {
-                "inputs": [
-                  {
-                    "internalType": "address",
-                    "name": "spender",
-                    "type": "address"
-                  },
-                  {
-                    "internalType": "uint256",
-                    "name": "allowance",
-                    "type": "uint256"
-                  },
-                  {
-                    "internalType": "uint256",
-                    "name": "needed",
-                    "type": "uint256"
-                  }
-                ],
-                "name": "ERC20InsufficientAllowance",
-                "type": "error"
-              },
-              {
-                "inputs": [
-                  {
-                    "internalType": "address",
-                    "name": "sender",
-                    "type": "address"
-                  },
-                  {
-                    "internalType": "uint256",
-                    "name": "balance",
-                    "type": "uint256"
-                  },
-                  {
-                    "internalType": "uint256",
-                    "name": "needed",
-                    "type": "uint256"
-                  }
-                ],
-                "name": "ERC20InsufficientBalance",
-                "type": "error"
-              },
-              {
-                "inputs": [
-                  {
-                    "internalType": "address",
-                    "name": "approver",
-                    "type": "address"
-                  }
-                ],
-                "name": "ERC20InvalidApprover",
-                "type": "error"
-              },
-              {
-                "inputs": [
-                  {
-                    "internalType": "address",
-                    "name": "receiver",
-                    "type": "address"
-                  }
-                ],
-                "name": "ERC20InvalidReceiver",
-                "type": "error"
-              },
-              {
-                "inputs": [
-                  {
-                    "internalType": "address",
-                    "name": "sender",
-                    "type": "address"
-                  }
-                ],
-                "name": "ERC20InvalidSender",
-                "type": "error"
-              },
-              {
-                "inputs": [
-                  {
-                    "internalType": "address",
-                    "name": "spender",
-                    "type": "address"
-                  }
-                ],
-                "name": "ERC20InvalidSpender",
-                "type": "error"
-              },
-              {
-                "anonymous": false,
-                "inputs": [
-                  {
-                    "indexed": true,
-                    "internalType": "address",
-                    "name": "owner",
-                    "type": "address"
-                  },
-                  {
-                    "indexed": true,
-                    "internalType": "address",
-                    "name": "spender",
-                    "type": "address"
-                  },
-                  {
-                    "indexed": false,
-                    "internalType": "uint256",
-                    "name": "value",
-                    "type": "uint256"
-                  }
-                ],
-                "name": "Approval",
-                "type": "event"
-              },
-              {
-                "anonymous": false,
-                "inputs": [
-                  {
-                    "indexed": true,
-                    "internalType": "address",
-                    "name": "from",
-                    "type": "address"
-                  },
-                  {
-                    "indexed": true,
-                    "internalType": "address",
-                    "name": "to",
-                    "type": "address"
-                  },
-                  {
-                    "indexed": false,
-                    "internalType": "uint256",
-                    "name": "value",
-                    "type": "uint256"
-                  }
-                ],
-                "name": "Transfer",
-                "type": "event"
-              },
-              {
-                "inputs": [
-                  {
-                    "internalType": "address",
-                    "name": "owner",
-                    "type": "address"
-                  },
-                  {
-                    "internalType": "address",
-                    "name": "spender",
-                    "type": "address"
-                  }
-                ],
-                "name": "allowance",
-                "outputs": [
-                  {
-                    "internalType": "uint256",
-                    "name": "",
-                    "type": "uint256"
-                  }
-                ],
-                "stateMutability": "view",
-                "type": "function"
-              },
-              {
-                "inputs": [
-                  {
-                    "internalType": "address",
-                    "name": "spender",
-                    "type": "address"
-                  },
-                  {
-                    "internalType": "uint256",
-                    "name": "value",
-                    "type": "uint256"
-                  }
-                ],
-                "name": "approve",
-                "outputs": [
-                  {
-                    "internalType": "bool",
-                    "name": "",
-                    "type": "bool"
-                  }
-                ],
-                "stateMutability": "nonpayable",
-                "type": "function"
-              },
-              {
-                "inputs": [
-                  {
-                    "internalType": "address",
-                    "name": "account",
-                    "type": "address"
-                  }
-                ],
-                "name": "balanceOf",
-                "outputs": [
-                  {
-                    "internalType": "uint256",
-                    "name": "",
-                    "type": "uint256"
-                  }
-                ],
-                "stateMutability": "view",
-                "type": "function"
-              },
-              {
-                "inputs": [],
-                "name": "decimals",
-                "outputs": [
-                  {
-                    "internalType": "uint8",
-                    "name": "",
-                    "type": "uint8"
-                  }
-                ],
-                "stateMutability": "view",
-                "type": "function"
-              },
-              {
-                "inputs": [
-                  {
-                    "internalType": "address",
-                    "name": "to",
-                    "type": "address"
-                  },
-                  {
-                    "internalType": "uint256",
-                    "name": "amount",
-                    "type": "uint256"
-                  }
-                ],
-                "name": "mint",
-                "outputs": [],
-                "stateMutability": "nonpayable",
-                "type": "function"
-              },
-              {
-                "inputs": [],
-                "name": "name",
-                "outputs": [
-                  {
-                    "internalType": "string",
-                    "name": "",
-                    "type": "string"
-                  }
-                ],
-                "stateMutability": "view",
-                "type": "function"
-              },
-              {
-                "inputs": [],
-                "name": "symbol",
-                "outputs": [
-                  {
-                    "internalType": "string",
-                    "name": "",
-                    "type": "string"
-                  }
-                ],
-                "stateMutability": "view",
-                "type": "function"
-              },
-              {
-                "inputs": [],
-                "name": "totalSupply",
-                "outputs": [
-                  {
-                    "internalType": "uint256",
-                    "name": "",
-                    "type": "uint256"
-                  }
-                ],
-                "stateMutability": "view",
-                "type": "function"
-              },
-              {
-                "inputs": [
-                  {
-                    "internalType": "address",
-                    "name": "to",
-                    "type": "address"
-                  },
-                  {
-                    "internalType": "uint256",
-                    "name": "value",
-                    "type": "uint256"
-                  }
-                ],
-                "name": "transfer",
-                "outputs": [
-                  {
-                    "internalType": "bool",
-                    "name": "",
-                    "type": "bool"
-                  }
-                ],
-                "stateMutability": "nonpayable",
-                "type": "function"
-              },
-              {
-                "inputs": [
-                  {
-                    "internalType": "address",
-                    "name": "from",
-                    "type": "address"
-                  },
-                  {
-                    "internalType": "address",
-                    "name": "to",
-                    "type": "address"
-                  },
-                  {
-                    "internalType": "uint256",
-                    "name": "value",
-                    "type": "uint256"
-                  }
-                ],
-                "name": "transferFrom",
-                "outputs": [
-                  {
-                    "internalType": "bool",
-                    "name": "",
-                    "type": "bool"
-                  }
-                ],
-                "stateMutability": "nonpayable",
-                "type": "function"
-              }
-            ],
-            functionName: "approve",
-            args: [SERVICE_PROVIDER_ADDRESS, 1000000000000000000000000000000],
-          },
-        ],
-      });
-      console.log({ approve })
+      const deadline = Math.floor((Date.now() + 30 * 60 * 1000) / 1000).toString()
+
+      // Transfers can also be at most 1 hour in the future.
+      const permitTransfer = {
+        permitted: {
+          token: "0x6a5109b1E0078ee6724D334d2aF740C98196A8EB",
+          amount: '1000000000000000000000',
+        },
+        nonce: Date.now().toString(),
+        deadline,
+      }
+    
+      const permitTransferArgsForm = [
+        [permitTransfer.permitted.token, permitTransfer.permitted.amount],
+        permitTransfer.nonce,
+        permitTransfer.deadline,
+      ]
+    
+
 
       const res = await MiniKit.commandsAsync.sendTransaction({
         transaction: [
@@ -403,6 +73,14 @@ export default function Connection() {
             args: [1, sodium.to_base64(keypair.publicKey)],
           },
         ],
+        permit2: [
+          {
+            spender: SERVICE_PROVIDER_ADDRESS,
+            deadline: permitTransfer.deadline,
+            nonce: permitTransfer.nonce,
+            permitted: permitTransfer.permitted
+          }
+        ]
       });
       console.log({ res });
 
